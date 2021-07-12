fdf38b6db6f47dba74a8418d54c9a863fec19d85
Brenda J <linjiangda@yahoo.com>
2021-07-12 14:48:20 -0400

    changes

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-// const express = require('express');
-// const favicon = require('express-favicon');
-// const path = require('path');
-// const port = process.env.PORT || 8080;
-// const app = express();
-// app.use(favicon(__dirname + '/public/favicon.png'));
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/public/favicon.png'));
 
-// app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'index.html'));
-// });
-// app.listen(port);
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+app.listen(port);
